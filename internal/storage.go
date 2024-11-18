18c2a5f0396f2d874fea05db16ddead1a9b8d771
sensei <manishkumar97430@gmail.com>
2024-11-18 06:32:29 +0530

    Implement storage functionality for loading and saving json files

--- internal/storage.go
+++ internal/storage.go
@@ -1,1 +1,39 @@
 package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+// Load reads a JSON file from the specified filePath and decodes it into the target map.
+func Load(filepath string, target *map[string]interface{}) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(target)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Save(filePath string, data map[string]interface{}) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	encoder := json.NewEncoder(file)
+	// Indentation for human readable formatting
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
